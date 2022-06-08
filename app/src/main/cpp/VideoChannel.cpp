3b5b42e21bb3229ebb76ad8c8e3f457daad3e165
liupanfeng <liupanfeng529@163.com>
2022-06-08 17:36:47 +0800

    梳理逻辑，完善日志

--- app/src/main/cpp/VideoChannel.cpp
+++ app/src/main/cpp/VideoChannel.cpp
@@ -2,6 +2,9 @@
 //
 #include "VideoChannel.h"
 
+#define STATE_WORK 1
+#define STATE_STOP 0
+
 
 /**
  * 丢包 AVFrame * 原始包 很简单，因为不需要考虑 关键帧
@@ -68,24 +71,27 @@ void *task_video_play(void *args) {
     return nullptr;
 }
 
-// 视频：1.解码    2.播放
-// 1.把队列里面的压缩包(AVPacket *)取出来，然后解码成（AVFrame * ）原始包 ----> 保存队列
-// 2.把队列里面的原始包(AVFrame *)取出来， 播放
+/**
+ * 1.把队列里面的压缩包(AVPacket *)取出来，然后解码成（AVFrame * ）原始包 ----> 保存队列
+ * 2.把队列里面的原始包(AVFrame *)取出来， 播放
+ */
 void VideoChannel::start() {
     isPlaying = true;
 
     // 队列开始工作了
-    packets.setWork(1);
-    frames.setWork(1);
+    packets.setWork(STATE_WORK);
+    frames.setWork(STATE_WORK);
 
-    // 第一个线程： 视频：取出队列的压缩包 进行解码 解码后的原始包 再push队列中去
-    pthread_create(&pid_video_decode, 0, task_video_decode, this);
+    //开线程->视频,取出队列的压缩包 进行解码 解码后的原始包 再push队列中去
+    pthread_create(&pid_video_decode, nullptr, task_video_decode, this);
 
-    // 第二线线程：视频：从队列取出原始包，播放
-    pthread_create(&pid_video_play, 0, task_video_play, this);
+    //开线程：视频，从队列取出原始包，播放
+    pthread_create(&pid_video_play, nullptr, task_video_play, this);
 }
 
-// 1.把队列里面的压缩包(AVPacket *)取出来，然后解码成（AVFrame * ）原始包 ----> 保存队列  【真正干活的就是他】
+/**
+ * 把队列里面的压缩包(AVPacket *)取出来，然后解码成（AVFrame * ）原始包 ----> 保存队列
+ */
 void VideoChannel::video_decode() {
     AVPacket *pkt = 0;
     while (isPlaying) {
@@ -99,62 +105,60 @@ void VideoChannel::video_decode() {
         }
 
         if (!ret) { // ret == 0
-            continue; // 哪怕是没有成功，也要继续（假设：你生产太慢(压缩包加入队列)，我消费就等一下你）
+            /* 继续（生产太慢(压缩包加入队列)，消费就等一下）*/
+            continue;
         }
 
-        // 最新的FFmpeg，和旧版本差别很大， 新版本：1.发送pkt（压缩包）给缓冲区，  2.从缓冲区拿出来（原始包）
+        /*1.发送pkt（压缩包）给缓冲区，  2.从缓冲区拿出来（原始包）*/
         ret = avcodec_send_packet(codecContext, pkt);
 
-        // FFmpeg源码缓存一份pkt，大胆释放即可
-//        releaseAVPacket(&pkt);
-
         if (ret) {
-            break; // avcodec_send_packet 出现了错误，结束循环
+            break;
         }
 
-        // 下面是从 FFmpeg缓冲区 获取 原始包
-        AVFrame *frame = av_frame_alloc(); // AVFrame： 解码后的视频原始数据包
+        /*下面是从 FFmpeg缓冲区 获取 原始包  AVFrame： 解码后的视频原始数据包*/
+        AVFrame *frame = av_frame_alloc();
         ret = avcodec_receive_frame(codecContext, frame);
         if (ret == AVERROR(EAGAIN)) {
-            // B帧  B帧参考前面成功  B帧参考后面失败   可能是P帧没有出来，再拿一次就行了
+            /*B帧  B帧参考前面成功  B帧参考后面失败 可能是P帧没有出来，再拿一次就行了*/
             continue;
         } else if (ret != 0) {
             if (frame){
                 releaseAVFrame(&frame);
             }
-            break; // 错误了
+            break;
         }
-        // 重要拿到了 原始包
+        /*将原始包放到 帧队列 */
         frames.insertToQueue(frame);
-        ///使用完 记得释放 否则会造成内容泄漏
+        /*使用完 记得释放 否则会造成内容泄漏*/
         av_packet_unref(pkt);
         releaseAVPacket(&pkt);
-    } // end while
-//    releaseAVPacket(&pkt);
+    }
+    /*发生异常释放 指针*/
     av_packet_unref(pkt);
     releaseAVPacket(&pkt);
 }
 
-// 2.把队列里面的原始包(AVFrame *)取出来， 播放 【真正干活的就是他】
-void VideoChannel::video_play() { // 第二线线程：视频：从队列取出原始包，播放 【真正干活了】
+/*把队列里面的原始包(AVFrame *)取出来播放*/
+void VideoChannel::video_play() {
 
-    // SWS_FAST_BILINEAR == 很快 可能会模糊
+    // SWS_FAST_BILINEAR：速度快可能会模糊
     // SWS_BILINEAR 适中算法
 
-    AVFrame *frame = 0;
+    AVFrame *frame = nullptr;
     uint8_t *dst_data[4]; // RGBA
     int dst_linesize[4]; // RGBA
     // 原始包（YUV数据）  ---->[libswscale]   Android屏幕（RGBA数据）
-    //给 dst_data 申请内存   width * height * 4 xxxx
+    //给 dst_data 申请内存   width * height * 4
     av_image_alloc(dst_data, dst_linesize,
                    codecContext->width, codecContext->height,
                    AV_PIX_FMT_RGBA, 1);
-    // yuv -> rgba
+    /* yuv -> rgba*/
     SwsContext *sws_ctx = sws_getContext(
             // 下面是输入环节
             codecContext->width,
             codecContext->height,
-            codecContext->pix_fmt, // 自动获取 xxx.mp4 的像素格式  AV_PIX_FMT_YUV420P // 写死的
+            codecContext->pix_fmt, // 自动获取 xxx.mp4 的像素格式  AV_PIX_FMT_YUV420P
 
             // 下面是输出环节
             codecContext->width,
@@ -165,10 +169,12 @@ void VideoChannel::video_play() { // 第二线线程：视频：从队列取出�
     while (isPlaying) {
         int ret = frames.getQueueAndDel(frame);
         if (!isPlaying) {
-            break; // 如果关闭了播放，跳出循环，releaseAVPacket(&pkt);
+            /*如果关闭了播放，跳出循环，releaseAVPacket(&pkt);*/
+            break;
         }
-        if (!ret) { // ret == 0
-            continue; // 哪怕是没有成功，也要继续（假设：你生产太慢(原始包加入队列)，我消费就等一下你）
+        if (!ret) {
+            /* 继续（生产太慢(压缩包加入队列)，消费就等一下）*/
+            continue;
         }
         // 格式转换 yuv ---> rgba
         sws_scale(sws_ctx,
@@ -181,7 +187,7 @@ void VideoChannel::video_play() { // 第二线线程：视频：从队列取出�
                   dst_linesize
         );
 
-        // 音视频同步（根据fps来休眠） FPS间隔时间加入（我的视频 默默的播放，不要看起来怪怪的） == 要有延时感觉
+        // 音视频同步（根据fps来休眠） FPS间隔时间加入 == 要有延时感觉
         // 0.04是这一帧的真实时间加上延迟时间吧
         // 公式：extra_delay = repeat_pict / (2*fps)
         // 经验值 extra_delay:0.0400000
@@ -190,25 +196,21 @@ void VideoChannel::video_play() { // 第二线线程：视频：从队列取出�
         double real_delay = fps_delay + extra_delay; // 当前帧的延时时间  0.040000
 
 
-        // fps间隔时间后的效果，任何播放器，都会有
-        // 为什么不能用：根据是 视频的 fps延时在处理，和音频还没有任何关系
-        // av_usleep(real_delay * 1000000);
+        // fps间隔时间后的效果，任何播放器都会有
 
-        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 下面是音视频同步
+        /*------------------------音视频同步----------------------------------------*/
         double video_time = frame->best_effort_timestamp * av_q2d(time_base);
         double audio_time = audio_channel->audio_time;
 
-        // 判断两个时间差值，一个快一个慢（快的等慢的，慢的快点追） == 你追我赶
+        /*判断两个时间差值，一个快一个慢（快的等慢的，慢的快点追）*/
         double time_diff = video_time - audio_time;
 
         if (time_diff > 0) {
-            // 视频时间 > 音频时间： 要等音频，所以控制视频播放慢一点（等音频） 【睡眠】
+            // 视频时间 > 音频时间： 要等音频，所以控制视频播放慢一点（等音频）
             if (time_diff > 1)
             {
-                // 说明：音频预视频插件很大， 拖动条 特色场景  音频 和 视频 差值很大，我不能睡眠那么久，否则是大Bug
-                // av_usleep((real_delay + time_diff) * 1000000);
-
-                // 如果 音频 和 视频 差值很大，我不会睡很久，我就是稍微睡一下
+                /*音频预视频插件很大， 拖动条 特色场景  音频 和 视频 差值很大，我不能睡眠那么久，否则是大Bug*/
+                /*如果音频和视频差值很大，稍微睡一下*/
                 av_usleep((real_delay * 2) * 1000000);
             }
             else
@@ -216,12 +218,9 @@ void VideoChannel::video_play() { // 第二线线程：视频：从队列取出�
                 av_usleep((real_delay + time_diff) * 1000000); // 单位是微妙：所以 * 1000000
             }
         } if (time_diff < 0) {
-            // 视频时间 < 音频时间： 要追音频，所以控制视频播放快一点（追音频） 【丢包】
-            // 丢帧：不能睡意丢，I帧是绝对不能丢
-            // 丢包：在frames 和 packets 中的队列
-
-            // 经验值 0.05
-            // -0.234454   fabs == 0.234454
+            /*丢包：在frames 和 packets 中的队列*/
+            /* 视频时间 < 音频时间： 要追音频，所以控制视频播放快一点（追音频） 【丢包】I帧是绝对不能丢 */
+            /*经验值 0.05 -0.234454   fabs == 0.234454*/
             if (fabs(time_diff) <= 0.05) { // fabs对负数的操作（对浮点数取绝对值）
                 // 多线程（安全 同步丢包）
                 frames.sync();
@@ -229,33 +228,21 @@ void VideoChannel::video_play() { // 第二线线程：视频：从队列取出�
             }
         } else {
             // 百分百同步，这个基本上很难做的
-            LOGI("百分百同步了");
+            LOGI("完全同步了");
         }
 
-        // 音视频同步3 video_time:9.840000, audio_time:9.792167, time_diff:0.047833
-        // 音视频同步3 video_time:9.880000, audio_time:9.856167, time_diff:0.023833
-        // 音视频同步3 video_time:9.920000, audio_time:9.877500, time_diff:0.042500
-        // 音视频同步3 video_time:9.960000, audio_time:9.920167, time_diff:0.039833
-        // 音视频同步3 video_time:10.000000, audio_time:9.962833, time_diff:0.037167
-        // 音视频同步3 video_time:10.040000, audio_time:10.005500, time_diff:0.034500
-        // 音视频同步3 video_time:10.080000, audio_time:10.048167, time_diff:0.031833
 
 
-        // ANatvieWindows 渲染工作  1   2下节课
-        // SurfaceView ----- ANatvieWindows
-        // 如何渲染一帧图像？
-        // 宽，高，数据  ----> 函数指针的声明
-        // 我拿不到Surface，只能回调给 native-lib.cpp
-
-        // 基础：数组被传递会退化成指针，默认就是去1元素
+        /*SurfaceView ----- ANatvieWindows NatvieWindows 渲染工作
+         * 拿不到Surface，只能回调给 native-lib.cpp*/
         renderCallback(dst_data[0], codecContext->width, codecContext->height, dst_linesize[0]);
         releaseAVFrame(&frame); // 释放原始包，因为已经被渲染完了，没用了
     }
-    // 简单的释放
-    releaseAVFrame(&frame); // 出现错误，所退出的循环，都要释放frame
+    /*出现错误，所退出的循环，都要释放frame*/
+    releaseAVFrame(&frame);
     isPlaying =false;
     av_free(&dst_data[0]);
-    sws_freeContext(sws_ctx); // free(sws_ctx); FFmpeg必须使用人家的函数释放，直接崩溃
+    sws_freeContext(sws_ctx);
 }
 
 void VideoChannel::setRenderCallback(RenderCallback renderCallback) {
