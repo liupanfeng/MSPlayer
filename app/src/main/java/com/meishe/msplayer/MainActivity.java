edaf20cc665fe68420b11a534dab78c431d3c056
lpf <569307092@qq.com>
2022-06-08 22:19:06 +0800

    设置封面图

--- app/src/main/java/com/meishe/msplayer/MainActivity.java
+++ app/src/main/java/com/meishe/msplayer/MainActivity.java
@@ -1,11 +1,20 @@
 package com.meishe.msplayer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.media.MediaMetadataRetriever;
+import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.SeekBar;
@@ -87,11 +96,49 @@ public class MainActivity extends AppCompatActivity implements MSPlayer.OnPrepar
                 Toast.makeText(MainActivity.this, "error:"+errorCode, Toast.LENGTH_SHORT).show();
             }
         });
-        mMSPlayer.setDataSource(
-                new File(Environment.
-                        getExternalStorageDirectory() +
-                        File.separator + "demo.mp4")
-                        .getAbsolutePath());
+
+        String filePath= new File(Environment.
+                getExternalStorageDirectory() +
+                File.separator + "demo.mp4")
+                .getAbsolutePath();
+
+        mMSPlayer.setDataSource(filePath);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int screenWidth = dm.widthPixels;
+        int screenHeight = dm.heightPixels;
+
+
+        Bitmap videoThumb = getVideoThumbnail(filePath,screenWidth,
+                screenHeight, MediaStore.Video.Thumbnails.MINI_KIND);
+
+        mBinding.surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                if (holder==null){
+                    return;
+                }
+                Paint paint = new Paint();
+                paint.setAntiAlias(true);
+                paint.setStyle(Paint.Style.STROKE);
+                /*先锁住画布*/
+                Canvas canvas = holder.lockCanvas();
+                canvas.drawBitmap(videoThumb,0,0,paint);
+                holder.unlockCanvasAndPost(canvas);
+
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+            }
+        });
 
         mMSPlayer.setOnOnProgressListener(new MSPlayer.OnProgressListener() {
             @Override
@@ -221,5 +268,41 @@ public class MainActivity extends AppCompatActivity implements MSPlayer.OnPrepar
         return "" + seconds;
     }
 
+
+
+    /**
+     * 获取视频的第一帧缩略图
+     * 先通过ThumbnailUtils来创建一个视频的缩略图，然后再利用ThumbnailUtils来生成指定大小的缩略图。
+     * 如果想要的缩略图的宽和高都小于MICRO_KIND，则类型要使用MICRO_KIND作为kind的值，这样会节省内存。
+     * @param videoPath 视频的路径
+     * @param width 指定输出视频缩略图的宽度
+     * @param height 指定输出视频缩略图的高度度
+     * @param kind 参照MediaStore.Images(Video).Thumbnails类中的常量MINI_KIND和MICRO_KIND。
+     *      其中，MINI_KIND: 512 x 384，MICRO_KIND: 96 x 96
+     * @return 指定大小的视频缩略图
+     */
+    public static Bitmap getVideoThumbnail(String videoPath, int width, int height, int kind) {
+        Bitmap bitmap = null;
+        // 获取视频的缩略图
+        bitmap = ThumbnailUtils.createVideoThumbnail(videoPath, kind); //调用ThumbnailUtils类的静态方法createVideoThumbnail获取视频的截图；
+        if(bitmap!= null){
+            bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height,
+                    ThumbnailUtils.OPTIONS_RECYCLE_INPUT);//调用ThumbnailUtils类的静态方法extractThumbnail将原图片（即上方截取的图片）转化为指定大小；
+        }
+        return bitmap;
+    }
+
+
+    /**
+     * 获取视频文件第一帧图
+     *
+     * @param path 视频文件的路径
+     * @return Bitmap 返回获取的Bitmap
+     */
+    public static Bitmap getVideoThumb(String path) {
+        MediaMetadataRetriever media = new MediaMetadataRetriever();
+        media.setDataSource(path);
+        return media.getFrameAtTime();
+    }
 
 }
